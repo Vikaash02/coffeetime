001d838ed21076305728dd731355da8ac947f642
Vikaash Harichandran <jbvikaash@gmail.com>
2021-10-04 02:38:31 +0800

    Update integer.cpp

--- variable/integer/signed/integer.cpp
+++ variable/integer/signed/integer.cpp
@@ -1,5 +1,5 @@
 int main() {
-    // Each integer is allocated 4 bytes of memory
+    // Each integer is allocated 4 bytes
     // Can be from -2,147,483,648 to 2,147,483,647
     int integer_var = 10;
 
