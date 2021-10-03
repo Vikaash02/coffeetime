f9636b7d679c5e973f79b3300f2cacfc941f32b4
Vikaash Harichandran <jbvikaash@gmail.com>
2021-10-04 02:56:34 +0800

    Create long-long-integer.cpp

---
+++ variable/integer/unsigned/long-long-integer.cpp
@@ -0,0 +1,7 @@
+int main() {
+    // Each unsigned long long integer is allocated 8 bytes
+    // Can be from 0 to 18,446,744,073,709,551,615
+    unsigned long long int integer_var = 10;
+
+    return 0;
+}
